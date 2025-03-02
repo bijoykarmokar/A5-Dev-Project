a1ec5acc9e95f4e7349e4126c426857197dca4ef
Bijoykarmokar <bijoykarmokar798@gmail.com>
2025-03-02 21:35:06 +0600

    time add, aleart add and another section added

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,7 @@ document.getElementById("blog-button").addEventListener("click", function () {
 });
 
 const date = new Date();
+console.log(date.getTime())
 const fixDate = document.getElementById("fix-date");
 fixDate.innerText = date.toDateString();
 
@@ -15,6 +16,7 @@ const mainBody = document.getElementById("main-body");
 for (let i = 0; i < totalButton.length; i++) {
   let button = totalButton[i];
   button.addEventListener("click", function (e) {
+    alert("Board updated successfully.");
     let headerCount = parseInt(headerCountElement.innerText);
     let sectionCount = parseInt(sectionCountElement.innerText);
 
@@ -28,17 +30,26 @@ for (let i = 0; i < totalButton.length; i++) {
     const btn = document.getElementById(`${e.target.id}`);
     const heading = document.getElementById(`title-${e.target.id}`).innerText;
     const par = document.createElement("p");
-    par.innerText = heading;
+    par.innerText ="You have completed the task " + heading + " at " + date.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", hour12: true})
+    
     par.classList.add(
       "text-xl",
-      "font-bold",
-      "text-gray-300",
+      "font-medium",
+      "text-gray-600",
       "bg-slate-100",
-      "p-2"
+      "p-2",
+      "mt-10"
     );
     historyDiv.appendChild(par);
     btn.classList.add("disabled");
     btn.disabled = true;
   });
 }
+document.getElementById("clear-btn").addEventListener("click", function (e) {
+  historyDiv.innerHTML = "";
+});
+let bgArr=["bg-red-100","bg-green-100","bg-yellow-100","bg-orange-100","bg-blue-100","bg-tomato-100","bg-gray-100","bg-cyan-100","bg-purple-100","bg-pink-100"];
 
+document.getElementById("theme-btn").addEventListener("click", function (e) {
+    mainBody.classList.add(`${bgArr[Math.round(Math.random()*9)]}`) 
+});
